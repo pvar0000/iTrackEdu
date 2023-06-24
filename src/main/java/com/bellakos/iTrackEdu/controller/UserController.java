ca762a4f9721c53b6180ef4590df9da62c2cdfae
Pedro Vargas <vargas.pedro.dd@gmail.com>
2023-06-23 23:40:46 -0500

    Uso de argon2 para el cifrado por hash y las verificaciones de las passwords

--- src/main/java/com/bellakos/iTrackEdu/controller/UserController.java
+++ src/main/java/com/bellakos/iTrackEdu/controller/UserController.java
@@ -2,6 +2,8 @@ package com.bellakos.iTrackEdu.controller;
 
 import com.bellakos.iTrackEdu.dao.UserDAO;
 import com.bellakos.iTrackEdu.models.User;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +20,13 @@ public class UserController {
         return userDAO.getUsers();
     }
 
+    //Use argon2 to hash the password before saving it to the database
     @RequestMapping(value = "api/users", method = RequestMethod.POST)
     public void registerUser(@RequestBody User user) {
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        char[] password = user.getPassword().toCharArray(); // Convert to char[] before hashing
+        String hash = argon2.hash(1, 1024, 1, password);
+        user.setPassword(hash);
         userDAO.registerUser(user);
     }
 
--- src/main/java/com/bellakos/iTrackEdu/dao/UserDAOImp.java
+++ src/main/java/com/bellakos/iTrackEdu/dao/UserDAOImp.java
@@ -1,6 +1,8 @@
 package com.bellakos.iTrackEdu.dao;
 
 import com.bellakos.iTrackEdu.models.User;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
@@ -35,13 +37,18 @@ public class UserDAOImp implements UserDAO{
 
     @Override
     public boolean authenticate(User user) {
-        String query = "FROM User WHERE email = :email AND password = :password";
+        String query = "FROM User WHERE email = :email";
         @SuppressWarnings("unchecked") List<User> resultList = entityManager.createQuery(query)
                 .setParameter("email", user.getEmail())
-                .setParameter("password", user.getPassword())
                 .getResultList();
 
-        return !resultList.isEmpty();
+        if (resultList.isEmpty()) return false;
+
+        String password = resultList.get(0).getPassword();
+
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+
+        return argon2.verify(password, user.getPassword().toCharArray()); // Convert to char[] user.getPassword() before hashing
     }
 
 
